35d5ec29378965979834aec9c0ea695ea4bf245d
norman <normanchifenga@gmail.com>
2022-10-15 10:17:32 +0200

    start using Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Headers from "./components/Header";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
+import { useState} from 'react'
+import { Routes, Route, Navigate} from 'react-router-dom'
+import { Homepage } from './components/homefiles/index'
+import Footer from './components/Footer'
+import Cart from './components/Cart'
+import Cakes from './components/Cakes'
+import Default from './components/Default'
+import Login from './components/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Provider store={store}>
+               <Headers />
+                <Routes>
+                    <Route path="/" element = {<Navigate to="/home"/>}/>
+                    <Route path="/home" element = {<Homepage />}/>
+                    <Route path="/cart" element = {<Cart />}/>
+                    <Route path="/cake" element = {<Cakes />}/>
+                    <Route path="/login" element = {<Login />}/>
+                    <Route path="*" element = {<Default />}/>
+                </Routes>
+                <Footer />
+        </Provider>
+    );
+};
 
 export default App;
+
+            
+
